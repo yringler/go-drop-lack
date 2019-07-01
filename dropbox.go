8919af1153bfe3a586ff1d628f24c3859c1b6317
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-07-01 13:18:22 -0400

    add upload functionality

--- dropbox.go
+++ dropbox.go
@@ -3,9 +3,13 @@ package lackdr
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type TemporaryLinkReponse struct {
@@ -56,4 +60,59 @@ func MakeDropRequest(url string, body string) (*http.Response, error) {
 	}
 
 	return response, nil
+}
+
+func createUploadRequest(fileName, dropboxPath string) (*http.Request, error) {
+	const dropboxAPI = "https://content.dropboxapi.com/2/files/upload"
+
+	// Reopen the file before each request.
+	// When the request is made, the file is closed.
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	request, _ := http.NewRequest("POST", dropboxAPI, file)
+
+	request.Header.Add("Content-Type", "application/octet-stream")
+	request.Header.Add("Authorization", "Bearer "+AccessToken)
+	request.Header.Add("Dropbox-API-Arg", "{\"mode\":\"overwrite\",\"path\":\""+dropboxPath+file.Name()+"\"}")
+
+	return request, nil
+}
+
+// UploadFile uploads file to dropbox, return the dropbox path.
+func UploadFile(file, dropboxPath string) (string, error) {
+
+	// Make request until not rate-limited
+	for i := 0; i < 3; i++ {
+		request, err := createUploadRequest(file, dropboxPath)
+		if err != nil {
+			return "", err
+		}
+
+		response, err := http.DefaultClient.Do(request)
+		if err != nil {
+			fmt.Println(err)
+			return "", err
+		}
+
+		retry := response.Header.Get("Retry-After")
+
+		if retry != "" {
+			retrySeconds, _ := strconv.ParseInt(retry, 10, 32)
+			fmt.Println("retrying: ", retrySeconds, file)
+			time.Sleep(time.Duration(retrySeconds) * time.Second)
+		} else {
+			body, _ := ioutil.ReadAll(response.Body)
+
+			bodyString := string(body)
+			if strings.Contains(bodyString, "error") {
+				fmt.Println("upload error body:\n", string(body))
+			}
+			break
+		}
+	}
+
+	return dropboxPath + file, nil
 }
